cfeb53b02e1fea83c4ea6fd15d424cd2cb3fe069
Rebecca <rebecca@casecommons.org>
2019-04-06 17:26:01 -0400

    Borrowing heavily from todo app

--- index.js
+++ index.js
@@ -10,26 +10,117 @@
 // start date to end date
 // update automatically if there's a stop date
 
-function calculate(startDate, stopDate) {
 
+function calculate(startDate, stopDate, existingTime) {
+  existingTime = existingTime || 0;
+  timeAdded = stopDate - startDate;
+  return existingTime + timeAdded;
 }
 
-var calculator = new Vue({
-  el: "#calculator",
-  data: { 
+// dates
+//   date
+//     moment.format()
+//     start/continue/end
+// calculation happens only on stop or calculate (as of today)
+
+// logic: loop through dates
+//        if labeled start, create a start date
+//        if find a stop, create an end date, calculate time
+// Full spec-compliant TodoMVC with localStorage persistence
+// and hash-based routing in ~120 effective lines of JavaScript.
+
+// localStorage persistence
+var STORAGE_KEY = '3030calculator-v0.1'
+var dateStorage = {
+  fetch: function () {
+    var dates = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    dates.forEach(function (date, index) {
+      date.id = index
+    })
+    dateStorage.uid = dates.length
+    return dates
   },
+  save: function (dates) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(dates))
+  }
+}
+
+// app Vue instance
+var app = new Vue({
+  // app initial state
+  data: {
+    dates: dateStorage.fetch(),
+    newDate: '',
+    editedDate: null
+  },
+
+  // watch dates change for localStorage persistence
+  watch: {
+    dates: {
+      handler: function (dates) {
+        dateStorage.save(dates)
+      },
+      deep: true
+    }
+  },
+
+  // computed properties
+  // http://vuejs.org/guide/computed.html
+  //computed: {
+    //filteredTodos: function () {
+      //return filters[this.visibility](this.todos)
+    //},
+    //remaining: function () {
+      //return filters.active(this.todos).length
+    //},
+    //allDone: {
+      //get: function () {
+        //return this.remaining === 0
+      //},
+      //set: function (value) {
+        //this.todos.forEach(function (todo) {
+          //todo.completed = value
+        //})
+      //}
+    //}
+  //},
+
+  filters: {
+    pluralize: function (n) {
+      return n === 1 ? 'date' : 'dates'
+    }
+  },
+
+  // methods that implement data logic.
+  // note there's no DOM manipulation here at all.
   methods: {
-    addStartDate: function(input) {
-      //will need to format the input
-      var start = moment.format();
-    },
-    addEndDate: function(input) {
-      //will need to format the input
-      var end = moment.format(input)
-    },
-    calculateOnDemand: function() {
-      var end = moment();
-      return calculate(start, end);
+    addDate: function () {
+      var value = this.newDate
+      var dateType = this.buttonClass
+      if (!value) {
+        return
+      }
+      this.dates.push({
+        id: dateStorage.uid++,
+        date: moment().format(value),
+        dateType: dateType
+      })
+      this.newDate = ''
     }
   }
+  //, 
+  // a custom directive to wait for the DOM to be updated
+  // before focusing on the input field.
+  // http://vuejs.org/guide/custom-directive.html
+  //directives: {
+    //'todo-focus': function (el, binding) {
+      //if (binding.value) {
+        //el.focus()
+      //}
+    //}
+  //}
 })
+
+// mount
+app.$mount('#thirtythirtyapp')
+
